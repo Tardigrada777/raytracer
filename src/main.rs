dc79e89af3be248411de6235bb93cc337a03d8e6
tardigrada777 <ntwrite@gmail.com>
2021-11-09 18:05:57 +0300

    implement lambertian material

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rand::prelude::*;
+
+mod camera;
 mod hittable;
 mod hittable_list;
 mod ray;
@@ -5,28 +8,12 @@ mod sphere;
 mod vec3;
 
 // TODO: move this into lib.rs
+use camera::Camera;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
-
-// fn write_ppm(w: i32, h: i32, max_value: i32) {
-//     println!("P3\n{} {}\n{}", w, h, max_value);
-//     for j in (0..h).rev() {
-//         for i in 0..w {
-//             let r: f32 = i as f32 / w as f32;
-//             let g: f32 = j as f32 / h as f32;
-//             let b: f32 = 0.2;
-
-//             let ir: i32 = (255.99 * r) as i32;
-//             let ig: i32 = (255.99 * g) as i32;
-//             let ib: i32 = (255.99 * b) as i32;
-
-//             println!("{} {} {}", ir, ig, ib);
-//         }
-//     }
-// }
 
 fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin() - center;
@@ -44,11 +31,8 @@ fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
 fn color(r: &Ray, world: &HittableList) -> Vec3 {
     let mut rec = HitRecord::default();
     if world.hit(&r, 0.0, std::f32::MAX, &mut rec) {
-        return Vec3::new(
-            rec.normal().x() + 1.0,
-            rec.normal().y() + 1.0,
-            rec.normal().z() + 1.0,
-        ) * 0.5;
+        let target = rec.p() + rec.normal() + random_in_unit_sphere();
+        return 0.5 * color(&Ray::new(rec.p(), target - rec.p()), world);
     } else {
         let unit_direction = Vec3::unit_vector(&r.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -56,16 +40,26 @@ fn color(r: &Ray, world: &HittableList) -> Vec3 {
     }
 }
 
-fn main() {
-    let width: i32 = 800;
-    let height: i32 = 400;
-    let max_value: i32 = 255;
-    // write_ppm(width, height, max_value);
+fn random_in_unit_sphere() -> Vec3 {
+    let mut p = Vec3::default();
+    let mut rng = rand::thread_rng();
+    loop {
+        p = 2.0 * Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), rng.gen::<f32>())
+            - Vec3::new(1.0, 1.0, 1.0);
+        if p.squared_length() < 1.0 {
+            return p;
+        }
+    }
+}
 
-    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
-    let horizontal = Vec3::new(4.0, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, 2.0, 0.0);
-    let origin = Vec3::new(0.0, 0.0, 0.0);
+fn main() {
+    let width = 800;
+    let height = 400;
+    let samples = 100;
+    let max_value: i32 = 255;
+
+    let camera = Camera::new();
+    let mut rng = rand::thread_rng();
 
     let mut list: Vec<Box<dyn Hittable>> = Vec::new();
     list.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)));
@@ -75,14 +69,18 @@ fn main() {
 
     for j in (0..height).rev() {
         for i in 0..width {
-            let u: f32 = i as f32 / width as f32;
-            let v: f32 = j as f32 / height as f32;
-            let r = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
-            let p = r.point_at_parameter(2.0);
-            let color = color(&r, &world);
-            let ir: i32 = (255.99 * color.r()) as i32;
-            let ig: i32 = (255.99 * color.g()) as i32;
-            let ib: i32 = (255.99 * color.b()) as i32;
+            let mut col = Vec3::default();
+            for _ in 0..samples {
+                let u: f32 = (i as f32 + rng.gen::<f32>()) / width as f32;
+                let v: f32 = (j as f32 + rng.gen::<f32>()) / height as f32;
+                let r = &camera.get_ray(u, v);
+                col = col + color(&r, &world);
+            }
+            col = col / samples as f32;
+            col = Vec3::new(col.r().sqrt(), col.g().sqrt(), col.b().sqrt());
+            let ir: i32 = (255.99 * col.r()) as i32;
+            let ig: i32 = (255.99 * col.g()) as i32;
+            let ib: i32 = (255.99 * col.b()) as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
